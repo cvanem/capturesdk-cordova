959da9962881c4d3d83a8608196cf7c827eb40c2
Chris Van Emmerik <chris@greenlinkservices.com>
2018-09-06 18:11:16 -0600

    t

--- examples/clean.js
+++ examples/clean.js
@@ -5,3 +5,9 @@ fs.remove('./inappbrowser', err => {
   
     console.log('Remove ./inappbrowser success!') // I just deleted my entire HOME directory.
 })
+
+fs.remove('./capturesdk-cordova', err => {
+    if (err) return console.error(err)
+  
+    console.log('Remove ./capturesdk-cordova success!') // I just deleted my entire HOME directory.
+})
--- examples/inappbrowser/plugins/capturesdk-cordova/examples/clean.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs-extra')
-
-fs.remove('./inappbrowser', err => {
-    if (err) return console.error(err)
-  
-    console.log('Remove ./inappbrowser success!') // I just deleted my entire HOME directory.
-})
