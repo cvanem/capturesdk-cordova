50887db3942cfa08078f528fce646c953c4c9052
Chris Van Emmerik <chris@greenlinkservices.com>
2018-09-10 02:47:04 -0600

    t

--- examples/copy.js
+++ examples/copy.js
@@ -4,3 +4,7 @@ const fs = require('fs-extra')
 fs.copy('./index.js', './inappbrowser/www/js/index.js')
   .then(() => console.log('copy index.js success!'))
   .catch(err => console.error(err))
+
+  fs.copy('./Podfile', './inappbrowser/platforms/ios/Podfile')
+  .then(() => console.log('copy Podfile success!'))
+  .catch(err => console.error(err))
--- examples/inappbrowser/plugins/capturesdk-cordova/examples/copy.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs-extra')
-
-// Async with promises:
-fs.copy('./index.js', './inappbrowser/www/js/index.js')
-  .then(() => console.log('copy index.js success!'))
-  .catch(err => console.error(err))
