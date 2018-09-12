7d87fe73c918dca507d810ba9783ec78705966b7
Chris Van Emmerik <chris@greenlinkservices.com>
2018-09-12 03:06:19 -0600

    t

---
+++ examples/inappbrowser/plugins/capturesdk-cordova/README.md
@@ -0,0 +1,44 @@
+# capturesdk-cordova
+Cordova plugin for Socket Mobile Capture SDK
+
+***Currently under development ****
+
+git clone https://cvanem/capturesdk-cordova
+
+
+Example using cordova-plugin-inappbrowser:
+
+Prerequisites:
+    
+    yarn
+    node
+    npm
+    cordova (npm install -g cordova)
+    Android tools: Android Studio/android sdk/jdk
+
+Open command prompt and navigate to the /examples directory and run the commands:
+
+    yarn install
+    yarn clean (Only needed if the examples/inappbrowser directory exists and you want to run yarn build)
+    yarn build (creates the cordova project and configures all plugins and the android platform)
+    yarn android (compiles and runs the android app on a usb connected device or open emulator)
+
+*Note: The above example builds from the public repository so if you need to change the app key or developer id, you will need to clone the repo first and update the values before building.  Here are instructions for doing this:
+In the example directory run:
+    
+    1. yarn install
+    2. yarn clean
+    3. yarn clonerepo (this clones the public repository to /examples/capturesdk-cordova)
+    4. Open the /examples/capturesdk-cordova/plugin.xml file and make the necessary changes (see below for instructions)
+    5. yarn buildfromclone (builds the cordova example using the cloned repo instead of the public repo)
+    6. yarn android
+    
+Instructions for changing the app key or developer id:
+To change the app key or developer id, you can insert your APP_KEY and DEVELOPER_ID into the plugin.xml file.  You must do this after cloning the repo prior to building (see step 4 above).   Here is the section in the plugin.xml file you need to update:
+
+    <platform name="android">
+        <config-file parent="./application" target="AndroidManifest.xml">
+            <meta-data android:name="com.socketmobile.capture.APP_KEY" android:value="..."/>
+            <meta-data android:name="com.socketmobile.capture.DEVELOPER_ID" android:value="..."/>
+        </config-file>
+    </platform>
--- src/android/CaptureSDK.java
+++ src/android/CaptureSDK.java
@@ -23,8 +23,6 @@ import android.text.Layout;
 import android.text.StaticLayout;
 import android.util.Log;
 
-
-/*
 import com.socketmobile.capture.android.Capture;
 
 import com.socketmobile.capture.client.DeviceClient;
@@ -46,7 +44,6 @@ import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 import com.capturesdk.cordova.BuildConfig;
 
-*/
 /**
  * This class echoes a string called from JavaScript.
  */
@@ -88,6 +85,78 @@ public class CaptureSDK extends CordovaPlugin {
         pluginResult.setKeepCallback(true); // Keep callback
     }
 
+    @Subscribe(threadMode = ThreadMode.MAIN, sticky = true)
+    public void onCaptureDeviceStateChange(DeviceStateEvent event) {
+        System.out.println("-------------------------------onCaptureDeviceStateChange");
+        DeviceClient device = event.getDevice();
+        DeviceState state = event.getState();
+
+        switch(state.intValue()) {
+            case DeviceState.READY:
+                // Ready to use
+                break;
+            default:
+                // Device not ready for use
+        }
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN, sticky = true)
+    public void onCaptureServiceConnectionStateChange(ConnectionStateEvent event) {
+        System.out.println("-------------------------------onCaptureServiceConnectionStateChange");
+        ConnectionState state = event.getState();
+        CaptureClient client = event.getClient();
+
+        if (state.hasError()) {
+            CaptureError error = state.getError();
+            switch(error.getCode()) {
+                case CaptureError.COMPANION_NOT_INSTALLED:
+                /*
+                alert("Socket Mobile Companion must be installed to use your scanner") {
+                    positiveButton("Install") { // onClick
+                        Capture.installCompanion(this.cordova.getActivity())
+                    }
+                }
+                */
+                    break;
+                case CaptureError.SERVICE_NOT_RUNNING:
+                    if (state.isDisconnected()) {
+                        if (Capture.notRestartedRecently()) {
+                            //Capture.restart(this);
+                        } else {
+                            // Service keeps crashing - Reboot the host device and check for updates to Companion
+                        }
+                    }
+                    break;
+                case CaptureError.BLUETOOTH_NOT_ENABLED:
+                /*alert("Bluetooth must be enabled to use your scanner") {
+                    positiveButton("Enable") { // onClick
+                        // BLUETOOTH permission must be enabled in your AndroidManifest
+                        startActivity(Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE));
+                    }
+                }
+                */
+                    break;
+            }
+        }
+
+        switch(state.intValue()) {
+            case ConnectionState.CONNECTING:
+                // ...
+                break;
+            case ConnectionState.CONNECTED:
+                // ...
+                break;
+            case ConnectionState.READY:
+                // ...
+                break;
+            case ConnectionState.DISCONNECTING:
+                // ...
+                break;
+            case ConnectionState.DISCONNECTED:
+                // ...
+                break;
+        }
+    }
     
     /**
      * Executes the request and returns PluginResult.
@@ -106,22 +175,54 @@ public class CaptureSDK extends CordovaPlugin {
 
         //This registers the javascript callback.  The callback is currently stored in onDataContext        
         if(action.equals("registerCallback")) {
-            onDataContext = callbackContext; //register the callback            
+            onDataContext = callbackContext; //register the callback
+            
+            Capture.builder(cordova.getActivity().getApplicationContext())
+            //.enableLogging(BuildConfig.DEBUG)
+            .build();
+
+            //Attempt to implement connectionCallback interface
+            connectionCallback = new ConnectionCallback() {
+                public void onConnectionStateChanged(ConnectionState state) {
+                    System.out.println("-------------------------------Connection State Changed");
+                    System.out.println(state);
+                    //Temporarily commented out due to error
+                    /*switch(state.get()) {
+                        case ConnectionState.CONNECTING:
+                            // do something or nothing
+                            break;
+                        case ConnectionState.CONNECTED:
+                            // client is now usable
+                            break;
+                        case ConnectionState.DISCONNECTING:
+                            // only called when shutting down gracefully
+                            break;
+                        case ConnectionState.DISCONNECTED:
+                            if(state.disconnectCausedByError()) {
+                                // Handle error
+                            } else {
+                                // Shut down normally
+                            }
+                        default:
+                            // Unreachable
+                            break;
+                    }
+                    */
+                }
+            };            
+            
+            System.out.println("-------------------------------Capture builder done");
             cordova.getThreadPool().execute(new Runnable() {
-                public void run() {
+                public void run() {                    
+
                     //Set the app key and initialize capture client
-                    System.out.println("-------------------------------Capture builder started");
-                    //Capture.builder(cordova.getActivity().getApplicationContext())
-                    //.enableLogging(BuildConfig.DEBUG)
-                    //.build();            
-                    //System.out.println("-------------------------------Capture builder done");                    
-                    //System.out.println("-------------------------------setting appkey and client");
-                    //appkey = new AppKey("MC0CFHc4jhssCXc8FljtHDgOeiV3YZJjAhUAgu+FTZgrAjpFyEOcBnVfWzrs1LA=","android:com.capturesdk.cordova","43d33419-e8e6-4ec6-a1f2-c8f9e6b960c8"); 
-                    //client = new CaptureClient(appkey);
+                    System.out.println("-------------------------------setting appkey and client");
+                    appkey = new AppKey("MC0CFHc4jhssCXc8FljtHDgOeiV3YZJjAhUAgu+FTZgrAjpFyEOcBnVfWzrs1LA=","android:com.capturesdk.cordova","43d33419-e8e6-4ec6-a1f2-c8f9e6b960c8"); 
+                    client = new CaptureClient(appkey);
                     //Can't figure out out to import CaptureClient.Listner as it alows throws a compiler error when imported
                     //client.setListener(new com.socketmobile.capture.client.CaptureClient.Listner() {
                     //});                    
-                    //client.connect(connectionCallback); //set the connection callback
+                    client.connect(connectionCallback); //set the connection callback
 
                     System.out.println("-------------------------------Registering callback");
                     // Then you're allowed to execute more than twice a callback.
